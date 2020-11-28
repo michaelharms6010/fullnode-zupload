4de67d09341d42b02cfd0fcad870e74f4985b911
Michael Harms <michael.harms6010@gmail.com>
2020-11-28 14:19:44 -0600

    handles opid sucess

--- archiveTextToZec.py
+++ archiveTextToZec.py
@@ -2,6 +2,17 @@ import subprocess
 import time
 import json
 
+funded_zaddr = "zs1lw6n36z0nsvahtae9kzv45j5esg062pchhuwcx3s4y22fltaxpxwkamwqsafxm2le786klunr9s"
+
+
+def get_operation_status(opid)
+    status = json.loads(subprocess.check_output("zcash-cli z_getoperationstatus '\"" + opid + "\"]'").strip())
+    return status[0]
+
+def operation_succeeded(opid)
+    return get_operation_status(opid)["status"] == "success"
+
+
 # file = input("enter the name of the file that you'd like to save and share via the zcash blockchain: ")
 file = "targettext.txt"
 
@@ -22,12 +33,26 @@ print("After the sending completes, you can view your content using the viewing 
 
 count = 0
 while count < len(memos):
+    opid = False
+    confirmed_balance = float(subprocess.check_output("zcash-cli z_getbalance \"" + funded_zaddr + "\"", shell=True).strip())
+    print(confirmed_balance)
+
+    while confirmed_balance < 0.00001:
+        time.sleep(15)
+        confirmed_balance = float(subprocess.check_output("zcash-cli z_getbalance \"" + funded_zaddr + "\"", shell=True).strip())
+        print(confirmed_balance)
+
     memo_as_hex = memos[count].encode("hex")
-    try: 
-        new_tx_command = 'zcash-cli z_sendmany "' + new_zaddr + '" ' + '\'[{"address": "zs1lw6n36z0nsvahtae9kzv45j5esg062pchhuwcx3s4y22fltaxpxwkamwqsafxm2le786klunr9s" ,"amount": 0, "memo": "' + memo_as_hex + '"}]\''
-    except:
-        print("the send failed")
-    opid = subprocess.check_output(new_tx_command, shell=True)
+     
+    new_tx_command = 'zcash-cli z_sendmany "' + funded_zaddr + '" ' + '\'[{"address": "'+ new_zaddr +'" ,"amount": 0, "memo": "' + memo_as_hex + '"}]\' 1 0.00001'
+    prev_opid = opid
+    while prev_opid = opid:
+        if not opid or operation_succeeded(opid):
+            opid = subprocess.check_output(new_tx_command, shell=True)
+        else:
+            print("it looks like " + opid + " failed.")
+            time.sleep(15)
+
     if "opid" in opid:
         print(opid)
         count += 1
