9239ae1b1b92eb4bfa50d52252f73db8a5805120
Michael Harms <michael.harms6010@gmail.com>
2020-11-28 14:35:26 -0600

    work work

---
+++ noizemaker.py
@@ -0,0 +1,20 @@
+# Nothing to see here
+
+send_zaddrs = []
+# would be cool to just grab every wallet addr with a balance
+
+zaddrs = json.loads(subprocess.check_output("zcash-cli z_getaddresses", shell=True).strip())
+
+index = 0
+
+while True:
+    zaddr = zaddrs[index]
+    balance = float(subprocess.check_output("zcash-cli z_getbalance \"" + zaddr + "\")", shell=true).strip())
+    if balance >= 0.00001:
+        new_tx_command = 'zcash-cli z_sendmany "' + funded_zaddr + '" ' + '\'[{"address": "'+ new_zaddr +'" ,"amount": 0, "memo": "' + memo_as_hex + '"}]\' 1 0.00001'
+        opid = subprocess.check_output(new_tx_command, shell=True).strip()
+        time.sleep(5)
+        # send
+    else:
+        print("zero balance at " + zaddr)
+    index  = (index + 1) % len(zaddrs)
